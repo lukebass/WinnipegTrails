8a998b0c0e5ae33bda76904fbd8f7868dcbd3572
Luke J. Basso <lukejbass@gmail.com>
2015-07-07 00:03:43 -0700

    Leaderboard ranking

--- app/src/main/java/ca/winnipegtrails/winnipegtrails/UserListActivity.java
+++ app/src/main/java/ca/winnipegtrails/winnipegtrails/UserListActivity.java
@@ -2,17 +2,25 @@ package ca.winnipegtrails.winnipegtrails;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class UserListActivity extends Activity
 {
+    private Map<String, Integer> userRankMap = new HashMap<>();
     private ParseQueryAdapter<ParseUser> userQueryAdapter;
 
     @Override
@@ -32,6 +40,27 @@ public class UserListActivity extends Activity
         else {
             score.setText("0");
         }
+
+        ParseQuery<ParseUser> userQuery = ParseUser.getQuery();
+        userQuery.orderByDescending("points");
+
+        userQuery.findInBackground(new FindCallback<ParseUser>()
+        {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e)
+            {
+                if(e != null) {
+
+                    if(WinnipegTrailsApplication.APPDEBUG) {
+                        Log.d(WinnipegTrailsApplication.APPTAG, "An error occurred while querying for user eggs", e);
+                    }
+
+                    return;
+                }
+
+                createUserRankMap(objects);
+            }
+        });
 
         // Set up a customized query
         ParseQueryAdapter.QueryFactory<ParseUser> factory = new ParseQueryAdapter.QueryFactory<ParseUser>()
@@ -55,7 +84,7 @@ public class UserListActivity extends Activity
                 }
 
                 TextView title = (TextView) view.findViewById(R.id.list_item_title);
-                title.setText(item.getUsername());
+                title.setText(userRankMap.get(item.getObjectId()) + ".  " + item.getUsername());
 
                 TextView points = (TextView) view.findViewById(R.id.list_item_points);
                 if(item.getNumber("points") == null) {
@@ -75,6 +104,16 @@ public class UserListActivity extends Activity
         // Attach the query adapter to the view
         ListView postsListView = (ListView) findViewById(R.id.list);
         postsListView.setAdapter(userQueryAdapter);
+    }
+
+    private void createUserRankMap(List<ParseUser> objects)
+    {
+        int i = 1;
+        for(ParseUser item : objects) {
+
+            userRankMap.put(item.getObjectId(), i);
+            i++;
+        }
     }
 
     /*
