18483fa839c90d0b0bcf5203b8e661c36d6b9dda
Luke J. Basso <lukejbass@gmail.com>
2015-07-16 01:41:18 -0700

    Register activity

--- app/src/main/java/ca/winnipegtrails/winnipegtrails/VideoActivity.java
+++ app/src/main/java/ca/winnipegtrails/winnipegtrails/VideoActivity.java
@@ -1,6 +1,7 @@
 package ca.winnipegtrails.winnipegtrails;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class VideoActivity extends Activity
@@ -10,5 +11,8 @@ public class VideoActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.intro);
+        mediaPlayer.start();
     }
 }
