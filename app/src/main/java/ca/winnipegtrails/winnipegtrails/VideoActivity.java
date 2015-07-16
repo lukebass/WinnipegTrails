7a01ba8174615fd6345bf7cfea1e02010eeb1945
Luke J. Basso <lukejbass@gmail.com>
2015-07-16 02:25:45 -0700

    Intro video

--- app/src/main/java/ca/winnipegtrails/winnipegtrails/VideoActivity.java
+++ app/src/main/java/ca/winnipegtrails/winnipegtrails/VideoActivity.java
@@ -1,8 +1,11 @@
 package ca.winnipegtrails.winnipegtrails;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class VideoActivity extends Activity
 {
@@ -12,7 +15,20 @@ public class VideoActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.intro);
-        mediaPlayer.start();
+        VideoView videoView = (VideoView) findViewById(R.id.video);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro));
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp)
+            {
+                // Start an intent for the dispatch activity
+                Intent intent = new Intent(VideoActivity.this, ModeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        videoView.start();
     }
 }
