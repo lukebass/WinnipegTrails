dadaf3fe3831c20f7a8a2e4a531b221b879a37af
Luke J. Basso <lukejbass@gmail.com>
2015-07-21 01:19:52 -0700

    Add button handlers

--- app/src/main/java/ca/winnipegtrails/winnipegtrails/SettingsActivity.java
+++ app/src/main/java/ca/winnipegtrails/winnipegtrails/SettingsActivity.java
@@ -17,6 +17,33 @@ public class SettingsActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        TextView gemButton = (TextView) findViewById(R.id.gem_button);
+        gemButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                startActivity(new Intent(SettingsActivity.this, EggListActivity.class));
+            }
+        });
+
+        TextView leaderboardButton = (TextView) findViewById(R.id.leaderboard_button);
+        leaderboardButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                startActivity(new Intent(SettingsActivity.this, UserListActivity.class));
+            }
+        });
+
+        TextView cancelButton = (TextView) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+            }
+        });
+
         final TextView modeButton = (TextView) findViewById(R.id.mode_button);
         modeButton.setText(WinnipegTrailsApplication.types[WinnipegTrailsApplication.getTransportMode()]);
 
